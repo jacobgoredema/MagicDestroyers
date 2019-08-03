c744f6e426621576795159dbcb2cb871761593a7
Jay <Jay@DEV-PC>
2019-08-03 19:30:47 +0200

    Initial setup

---
+++ MagicDestroyers/Characters/Melee/Warrior.cs
@@ -0,0 +1,47 @@
+﻿using MagicDestroyers.Equipment.Armors.Heavy;
+using MagicDestroyers.Equipment.Weapons.Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicDestroyers.Characters.Melee
+{
+    public class Warrior
+    {
+        private int abilityPoints;
+        private int healthPoint;
+        private int level;
+        private string faction;
+        private string name;
+
+        private Chainlink bodyArmor;
+        private Axe weapon;
+
+        public Warrior()
+        {
+
+
+        }
+
+        public void Execute()
+        {
+
+        }
+
+        public void SkinHarden()
+        {
+
+        }
+
+        public void Strike()
+        {
+
+        }
+
+
+
+
+    }
+}
---
+++ MagicDestroyers/Characters/Spellcasters/Necromancer.cs
@@ -0,0 +1,42 @@
+﻿using MagicDestroyers.Equipment.Armors.Heavy;
+using MagicDestroyers.Equipment.Weapons.Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicDestroyers.Characters.Spellcasters
+{
+    public class Necromancer
+    {
+        private int abilityPoints;
+        private int healthPoint;
+        private int level;
+        private string faction;
+        private string name;
+
+        private Chainlink bodyArmor;
+        private Axe weapon;
+
+        public Necromancer()
+        {
+
+        }
+
+        public void Execute()
+        {
+
+        }
+
+        public void SkinHarden()
+        {
+
+        }
+
+        public void Strike()
+        {
+
+        }
+    }
+}
